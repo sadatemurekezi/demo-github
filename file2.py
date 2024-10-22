e2dc1919f1ae25b1b8451fed06b18144e82ce50f
sadate <murekezisadate@gmail.com>
2024-10-22 22:23:10 +0300

    New python file

---
+++ file2.py
@@ -0,0 +1,7 @@
+import numpy as np
+#import pandas as pd 
+
+a=input("enter the first number: ")
+b=input("enter the second number:")
+sum= float(a)+float(b)
+print("the sum of {0} and {1}is {2}".format(a, b,sum))
